3054f559bb89c8ef1d664a0aeed557f31fb3cd15
Aqsa Saleheene <angieclrk21@gmail.com>
2022-05-11 19:03:13 -0400

    Using async Handler

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,25 +1,28 @@
+//////// IMPORT ///////
+const asyncHandler = require('express-async-handler')
 
 
 
-const getBlogs = (req, res) => {
+
+const getBlogs = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Blogs " });
-};
+});
 
-const createBlog = (req, res) => {
-    if(!req.body.text) {
+const createBlog = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400)
         throw new Error("Please add text field")
   };
-  res.status(200).json({ message: "Create Blog "})
-}
+  res.status(200).json({ message: "Create Blog" })
+})
 
-  const updateBlog = (req, res) => {
+  const updateBlog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Blog ${req.params.id}` });
-  };
+  });
 
-  const deleteBlog = (req, res) => {
+  const deleteBlog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete Blog ${req.params.id}` });
-  };
+  });
 
   
 
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
+//////// IMPORT ////////
 const express = require("express");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
+const {errorHandler} = require('./middleware/errorMiddleware')
 
+
+/////// DEFINE VARIABLES ////////
+const port = process.env.PORT || 5000;
 const app = express();
 
+//////// MIDDLEWARE ////////
+
 app.use(express,json())
+
 app.use(express.urlendcoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/blogRoutes'))
 
+app.use(errorHandler)
+
+
+//////// PORT LISTENING ////////
 app.listen(port, () => console.log(`Server started on port ${port}`));

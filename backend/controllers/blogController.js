096defc5bd499d1092087067f9b790220b36de81
Aqsa Saleheene <angieclrk21@gmail.com>
2022-05-11 18:39:41 -0400

    Adding Middleware

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,10 +1,17 @@
+
+
+
 const getBlogs = (req, res) => {
   res.status(200).json({ message: "Get Blogs " });
 };
 
 const createBlog = (req, res) => {
-    res.status(200).json({ message: "Create Blog " });
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error("Please add text field")
   };
+  res.status(200).json({ message: "Create Blog "})
+}
 
   const updateBlog = (req, res) => {
     res.status(200).json({ message: `Update Blog ${req.params.id}` });
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express,json())
+app.use(express.urlendcoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/blogRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`));

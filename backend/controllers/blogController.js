4565b628b1430cd47310739e6836d634f794bd23
Aqsa Saleheene <angieclrk21@gmail.com>
2022-05-11 22:48:53 -0400

    Added findById

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,11 +1,13 @@
 //////// IMPORT ///////
 const asyncHandler = require('express-async-handler')
-
-
+const Blog = require('../models/blogModel')
 
 
 const getBlogs = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get Blogs " });
+const blogs = await Blog.find()
+
+
+  res.status(200).json(blogs);
 });
 
 const createBlog = asyncHandler(async (req, res) => {
@@ -13,15 +15,40 @@ const createBlog = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Please add text field")
   };
-  res.status(200).json({ message: "Create Blog" })
+
+  const blog = await Blog.create({
+      text: req.body.text
+  })
+
+  res.status(200).json(blog)
 })
 
   const updateBlog = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update Blog ${req.params.id}` });
+      const blog = await Blog.findById(req.params.id)
+
+      if (!blog) {
+          res.status(400)
+          throw new Error('Blog not found')
+      }
+
+     const updateBlog = await Blog.findOneAndUpdate(req.params.id, req.body, {
+         new: true,
+     } )
+
+    res.status(200).json(updateBlog);
   });
 
   const deleteBlog = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete Blog ${req.params.id}` });
+      const blog = await Blog.findById(req.params.id)
+
+      if (!blog) {
+          res.status(400)
+          throw new Error('Blog not found')
+      }
+
+     await blog.remove()
+
+    res.status(200).json({ id: req.params.id });
   });
 
   

0a0ffd6e30d2405506861c6b445f12902769da4a
Aqsa Saleheene <angieclrk21@gmail.com>
2022-05-11 23:36:19 -0400

    Added User Model

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,66 +1,66 @@
 //////// IMPORT ///////
-const asyncHandler = require('express-async-handler')
-const Blog = require('../models/blogModel')
+const asyncHandler = require("express-async-handler");
+const Blog = require("../models/Blog");
 
 
+
+//// GET BLOG ////
 const getBlogs = asyncHandler(async (req, res) => {
-const blogs = await Blog.find()
-
+  const blogs = await Blog.find();
 
   res.status(200).json(blogs);
 });
 
+//// CREATE BLOG ////
 const createBlog = asyncHandler(async (req, res) => {
-    if (!req.body.text) {
-        res.status(400)
-        throw new Error("Please add text field")
-  };
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add text field");
+  }
 
   const blog = await Blog.create({
-      text: req.body.text
-  })
-
-  res.status(200).json(blog)
-})
-
-  const updateBlog = asyncHandler(async (req, res) => {
-      const blog = await Blog.findById(req.params.id)
-
-      if (!blog) {
-          res.status(400)
-          throw new Error('Blog not found')
-      }
-
-     const updateBlog = await Blog.findOneAndUpdate(req.params.id, req.body, {
-         new: true,
-     } )
-
-    res.status(200).json(updateBlog);
+    text: req.body.text,
   });
 
-  const deleteBlog = asyncHandler(async (req, res) => {
-      const blog = await Blog.findById(req.params.id)
+  res.status(200).json(blog);
+});
 
-      if (!blog) {
-          res.status(400)
-          throw new Error('Blog not found')
-      }
+//// UPDATE BLOG ////
+const updateBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
 
-     await blog.remove()
+  if (!blog) {
+    res.status(400);
+    throw new Error("Blog not found");
+  }
 
-    res.status(200).json({ id: req.params.id });
+  const updateBlog = await Blog.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
   });
 
-  
+  res.status(200).json(updateBlog);
+});
 
 
+//// DELETE BLOG ////
+const deleteBlog = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
 
+  if (!blog) {
+    res.status(400);
+    throw new Error("Blog not found");
+  }
 
+  await blog.remove();
 
+  res.status(200).json({ id: req.params.id });
+});
+
+///// EXPORTING MODULES /////
 
 module.exports = {
-    getBlogs,
-    createBlog,
-    updateBlog,
-    deleteBlog
-}
+  getBlogs,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+};
---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+ const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your Name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your Email'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a Password'],
+        unique: true
+    },
+    username: {
+        type: String,
+        required: [true, 'Please enter a Username'],
+        unique: true
+    },
+},
+{
+    timestamps: true
+})

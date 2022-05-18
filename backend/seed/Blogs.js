657bb84b16ed13665d996f66492847ae7b6c437a
Aqsa Saleheene <angieclrk21@gmail.com>
2022-05-18 14:16:05 -0400

    Seeds

---
+++ backend/seed/Blogs.js
@@ -0,0 +1,31 @@
+const db = require('../db')
+const { Blog, User } = require('../models')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const user = await User.find({
+        name: "Jody Joo",
+    })
+  const blogs = [
+    {
+      title: 'post',
+      author: user[0]._id,
+      content: 'What a wonderful site',
+    },
+    {
+      title: 'post',
+      author: user[0]._id,
+      content: 'Nice'
+    }
+  ]
+
+  await Blog.insertMany(blogs)
+  console.log('Created blogs!')
+}
+const run = async () => {
+  await main()
+  db.close()
+}
+
+run()
